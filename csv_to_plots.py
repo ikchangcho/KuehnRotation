d0797ae39cf60a526d2c3271db2aad9b0df6eba9
ikchangcho <142732209+ikchangcho@users.noreply.github.com>
2024-07-08 15:40:52 -0500

    smooth derv

--- csv_to_plots.py
+++ csv_to_plots.py
@@ -19,34 +19,49 @@ def load_csv(filename_pattern, indices=None):
 
 
 # Function to process data, extracting specified columns and calculating derivatives
-def define_xy(data_list, init=0, x_col=0, x_factor=1, y_col=1, y_factor=1, filename=None):
+def define_xy(data_list, init=0, x_col=0, x_factor=1, y_col=1, y_factor=1, save_csv_filename=None, diff=False):
     xy_list = []
     for idx, data in enumerate(data_list):
-        x = data[init:, x_col] * x_factor
-        y = data[init:, y_col] * y_factor
+        if isinstance(x_factor, list):
+            x = data[init:, x_col] * x_factor[idx]
+        else:
+            x = data[init:, x_col] * x_factor
+        if isinstance(y_factor, list):
+            y = data[init:, y_col] * y_factor[idx]
+        else:
+            y = data[init:, y_col] * y_factor
+        if diff:
+            x = x - x[0]
+            y = y - y[0]
         xy_list.append(np.vstack((x, y)).T)
-        if filename is None:
-            filename = 'y_values'
-        df = pd.DataFrame({'x': x, 'y': y})
-        df.to_csv(f'{filename}_{idx}.csv', index=False)  # Save to CSV
+
+        if save_csv_filename is not None:
+            df = pd.DataFrame({'x': x, 'y': y})
+            df.to_csv(f'{save_csv_filename}_{idx}.csv', index=False)  # Save to CSV
     return xy_list
 
 
-def derivatives(xy_list):
+def derivatives(xy_list, save_csv=True):
     derivatives = []
     for idx, xy in enumerate(xy_list):
         x = xy[:, 0]
         y = xy[:, 1]
         dydx = np.gradient(y, x)  # Calculate the derivative
-        derivatives.append((x, dydx))
-        df = pd.DataFrame({'x': x, 'dydx': dydx})
-        df.to_csv(f'derivatives_{idx}.csv', index=False)  # Save to CSV
+        derivatives.append(np.vstack((x, dydx)).T)
+        if save_csv:
+            df = pd.DataFrame({'x': x, 'dydx': dydx})
+            df.to_csv(f'derivatives_{idx}.csv', index=False)  # Save to CSV
     return derivatives
 
 
-def smooth_derivatives(xy_list, half_window=10, poly_order=2):
+def smooth_derivatives(xy_list, half_window=5000, poly_order=2, indices=None, save_csv_filename=None):
     smooth_derivatives = []
-    for idx, xy in enumerate(xy_list):
+    if indices is None:
+        indices = range(len(xy_list))
+    xy_selected = [xy_list[i] for i in indices]
+
+    for idx, xy in enumerate(xy_selected):
+        xy = xy.astype(float)
         x = xy[:, 0]
         y = xy[:, 1]
         dydx = np.zeros(len(y))
@@ -56,13 +71,15 @@ def smooth_derivatives(xy_list, half_window=10, poly_order=2):
             # Fit polynomial and calculate the derivative at point x[i]
             p = np.polyfit(x[start:end], y[start:end], poly_order)
             dydx[i] = np.polyval(np.polyder(p), x[i])
-        smooth_derivatives.append((x, dydx))
-        df = pd.DataFrame({'x': x, 'dydx': dydx})
-        df.to_csv(f'smooth_derv_w{half_window}_{idx}.csv', index=False)  # Save to CSV
+        smooth_derivatives.append(np.vstack((x, dydx)).T)
+
+        if save_csv_filename is not None:
+            df = pd.DataFrame({'x': x, 'dydx': dydx})
+            df.to_csv(f'{save_csv_filename}_w{half_window}_{idx}.csv', index=False)  # Save to CSV
     return smooth_derivatives
 
 
-def plots_seperate(filename_pattern, indices=None, titles=None, xlabels=None, ylabels=None):
+def csv_to_plots_seperate(filename_pattern, indices=None, titles=None, xlabels=None, ylabels=None):
     data_list = load_csv(filename_pattern)
 
     if indices is None:
@@ -91,7 +108,7 @@ def plots_seperate(filename_pattern, indices=None, titles=None, xlabels=None, yl
     plt.show()
 
 
-def plots_overlap(filename_pattern, indices=None, title='', xlabel='', ylabel=''):
+def csv_to_plots_overlap(filename_pattern, indices=None, title='', xlabel='', ylabel='', legend=True, save_file_name=None):
     data_list = load_csv(filename_pattern)
 
     if indices is None:
@@ -106,41 +123,66 @@ def plots_overlap(filename_pattern, indices=None, title='', xlabel='', ylabel=''
     ax.set_title(f'{title}')
     ax.set_xlabel(f'{xlabel}')
     ax.set_ylabel(f'{ylabel}')
-    ax.legend()
+    if legend is True:
+        ax.legend()
     plt.tight_layout()
+    if save_file_name is not None:
+        fig.savefig(save_file_name)
     plt.show()
 
 
-def std_over_sensors():
-    filename_pattern = "020724_P*_calibrate_*.csv"
-    data_list = load_csv(filename_pattern)
-    xy_list = define_xy(data_list, x_col=2, x_factor=1 / 3600, y_col=3, filename='050724_CO2')
-    xy_list_truncated = []
-    for xy in xy_list:
-        xy_truncated = xy[xy[:, 0] >= xy[0, 0] + 19]
-        xy_truncated[:, 0] = xy_truncated[:, 0] - xy_truncated[0, 0]
-        xy_list_truncated.append(xy_truncated)
+def list_to_plots_overlap(data_list, indices=None, title='', xlabel='', ylabel='', legend=True, save_file_name=None):
+    if indices is None:
+        indices = range(len(data_list))
+    selected_data = [data_list[i] for i in indices]
 
-    times = np.arange(1.5, 44.7, 0.1)
-    dt = 1.5
-    avg_CO2_at_time_points = np.zeros((len(times), 19))
-    for j, xy in enumerate(xy_list_truncated):
-        x = xy[:, 0]
-        y = xy[:, 1]
-        for i, t0 in enumerate(times):
-            mask = (x > t0 - dt) & (x < t0 + dt)
-            avg_CO2_at_time_points[i, j] = np.average(y[mask])
-    variance = np.std(avg_CO2_at_time_points, axis=1)
-    np.savetxt('050724_avg_CO2_at_time_points.csv', avg_CO2_at_time_points, delimiter=",",
-               header=f'Average CO2 over dt = {dt} hour time span at every time point with interval 0.1 hour')
-    np.savetxt('050724_std.csv', variance, delimiter=",",
-               header='Standard deviation over 19 sensors at every time point with interval 0.1 hour')
-    plt.plot(times, variance)
-    plt.xlabel('Time after calibration (hours)')
-    plt.ylabel(r'$\Delta CO_2$ (ppm)')
-    plt.title(r'Standard deviation on $CO_2$ measurement over 19 sensors')
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+    for idx, data in enumerate(selected_data):
+        x = data[:, 0]  # assuming x values are in the first column
+        y = data[:, 1]  # assuming y values are in the second column
+        ax.plot(x, y, label=f'Data {indices[idx]}')
+    ax.set_title(f'{title}')
+    ax.set_xlabel(f'{xlabel}')
+    ax.set_ylabel(f'{ylabel}')
+    if legend is True:
+        ax.legend()
+    plt.tight_layout()
+    if save_file_name is not None:
+        fig.savefig(save_file_name)
     plt.show()
+
+
 
 # Example usage
 if __name__ == "__main__":
-    std_over_sensors()
+    filename_pattern1 = '260624_1cm2cm_*.csv'
+    data_list = load_csv(filename_pattern1)
+    xy_list = define_xy(data_list, x_col=2, x_factor=1/3600, y_col=3, y_factor=[1, 1/7.97, 1/5.30, 1/6.53, 1/16.54, 1/20.17, 1/20.03],
+                        diff=True, save_csv_filename='co2_per_gram_1cm2cm')
+    derv_list = smooth_derivatives(xy_list, save_csv_filename='derv_per_gram_1cm2cm')
+    list_to_plots_overlap(derv_list, xlabel='Time (hour)',
+                         ylabel=r'$\frac{d (\Delta CO_2)}{dt}$ (ppm/hour)', indices=[1, 2, 3],
+                         title=r'Time derivative of $\Delta CO_2$ per gram of soil' f'\n 1cm Stack', legend=False,
+                         save_file_name='080724_derv_1cm.png')
+    plt.close('all')
+    list_to_plots_overlap(derv_list, xlabel='Time (hour)',
+                         ylabel=r'$\frac{d (\Delta CO_2)}{dt}$ (ppm/hour)', indices=[4, 5, 6],
+                         title=r'Time derivative of $\Delta CO_2$ per gram of soil' f'\n 2cm Stack', legend=False,
+                         save_file_name='080724_derv_2cm.png')
+    plt.close('all')
+
+    filename_pattern2 = '260624_3cm4cm_*.csv'
+    data_list = load_csv(filename_pattern2)
+    xy_list = define_xy(data_list, x_col=2, x_factor=1/3600, y_col=3, y_factor=[1, 1/33.9, 1/31.67, 1/31.18, 1/45.08, 1/42.65, 1/46.93],
+                        diff=True, save_csv_filename='co2_per_gram_3cm4cm')
+    derv_list = smooth_derivatives(xy_list, save_csv_filename='derv_per_gram_3cm4cm')
+    list_to_plots_overlap(derv_list, xlabel='Time (hour)',
+                         ylabel=r'$\frac{d (\Delta CO_2)}{dt}$ (ppm/hour)', indices=[1, 2, 3],
+                         title=r'Time derivative of $\Delta CO_2$ per gram of soil' f'\n 3cm Stack', legend=False,
+                         save_file_name='080724_derv_3cm.png')
+    plt.close('all')
+    list_to_plots_overlap(derv_list, xlabel='Time (hour)',
+                         ylabel=r'$\frac{d (\Delta CO_2)}{dt}$ (ppm/hour)', indices=[4, 5, 6],
+                         title=r'Time derivative of $\Delta CO_2$ per gram of soil' f'\n 4cm Stack', legend=False,
+                         save_file_name='080724_derv_4cm.png')
+    plt.close('all')
